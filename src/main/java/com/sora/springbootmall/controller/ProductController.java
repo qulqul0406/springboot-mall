a70795f7caa8f0a27650e5d7f00d3d21ad7e5e87
sorahuang <sora.huang@nec.com.tw>
2023-12-28 10:36:23 +0800

    商品功能 - 修改annotation

--- src/main/java/com/sora/springbootmall/controller/ProductController.java
+++ src/main/java/com/sora/springbootmall/controller/ProductController.java
@@ -12,11 +12,12 @@ import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Valid
+@Validated
 @RestController
 public class ProductController {
 
